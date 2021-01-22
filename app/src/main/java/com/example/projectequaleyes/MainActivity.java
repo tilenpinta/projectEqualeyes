3eb09bd80e63008d97ed62e69f8f23ff384b895e
tilenpinta <tilen.pintaric@student.um.si>
2021-01-22 23:09:43 +0100

    Cleaning

--- app/src/main/java/com/example/projectequaleyes/MainActivity.java
+++ app/src/main/java/com/example/projectequaleyes/MainActivity.java
@@ -1,34 +1,15 @@
 package com.example.projectequaleyes;
 
-import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,12 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvTitle = findViewById(R.id.titleRed);
-        getTitles();
 
+        getTitles();
     }
 
     private void getTitles() {
-
         new Thread(new Runnable() {
             StringBuilder strBuild = new StringBuilder();
             @Override
@@ -53,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     Document doc = Jsoup.connect("https://www.reddit.com/").get();
                     Elements titles = doc.select("h3._eYtD2XCVieq6emjKBH3m");
-
                     int i = 1;
                     for (Element e : titles) {
 
@@ -64,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
                 } catch (Exception ex) {
                     ex.fillInStackTrace();
                 }
-
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -73,7 +51,5 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }).start();
-
     }
-
 }
